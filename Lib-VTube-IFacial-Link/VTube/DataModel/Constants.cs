42152c165e0f8bdbac0f1eb8fb0adc295d3da02a
瑄 <shanboxuan@me.com>
2023-01-04 02:52:25 +0000

    Update Plugin ID

--- Lib-VTube-IFacial-Link/VTube/DataModel/Constants.cs
+++ Lib-VTube-IFacial-Link/VTube/DataModel/Constants.cs
@@ -2,8 +2,8 @@
 {
     class Constants
     {
-        public const string PLUGIN_NAME = "VTubeIFacialLink-Sharp";
-        public const string PLUGIN_DEVELOPER = "xuan25";
+        public const string PLUGIN_NAME = "VTube-IFacial-Link";
+        public const string PLUGIN_DEVELOPER = "Xuan25";
         public const string PLUGIN_ICON = null;
     }
 }
