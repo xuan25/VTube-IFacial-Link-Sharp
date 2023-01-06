a8d0c96d30db9a90bcc015ab3b29b8a0e5c24ddf
瑄 <shanboxuan@me.com>
2023-01-06 22:44:35 +0000

    Fix data template bindings

---
+++ VTube-IFacial-Link-WinUI/Controls/RemovableItemsControl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VTube_IFacial_Link.Pages;
+
+namespace VTube_IFacial_Link.Controls
+{
+    class RemovableItemsControl : ItemsControl
+    {
+        public static readonly DependencyProperty RemoveCommandProperty = DependencyProperty.Register(nameof(RemoveCommand), typeof(ICommand), typeof(RemovableItemsControl), new PropertyMetadata(null));
+        public ICommand RemoveCommand
+        {
+            get { return (ICommand)GetValue(RemoveCommandProperty); }
+            set { SetValue(RemoveCommandProperty, value); }
+        }
+    }
+}
