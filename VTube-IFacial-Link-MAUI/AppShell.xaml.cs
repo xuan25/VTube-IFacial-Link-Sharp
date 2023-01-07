977179445ba0cdb6a4fc764690254f72b4248eb5
瑄 <shanboxuan@me.com>
2023-01-07 02:02:10 +0000

    Remove MAUI project

--- VTube-IFacial-Link-MAUI/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VTube_IFacial_Link;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- VTube-IFacial-Link-MAUI/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace VTube_IFacial_Link;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
