c40bceafe265fd229d1c7aeb438e7f2829744dcd
瑄 <shanboxuan@me.com>
2023-02-20 23:24:29 +0000

    Add launcher and publish configs

---
+++ Launch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace Launch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.CurrentDirectory = Path.Combine(Environment.CurrentDirectory, "bin");
+
+            Process process = new()
+            {
+                StartInfo = new ProcessStartInfo("VTube-IFacial-Link.exe")
+            };
+            process.Start();
+        }
+    }
+}
